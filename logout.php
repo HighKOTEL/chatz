92c4ea9f6dfa22717166270085b184666c1f6338
HighKOTEL <kizinizi274@gmail.com>
2025-03-09 11:54:27 +0300

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset(); // Удаляет все переменные сессии
-session_destroy(); // Уничтожает сессию
-
-header("Location: login.php"); // Перенаправляет на страницу входа
-exit();
-?>
